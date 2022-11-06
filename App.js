d4fa5308cf81254286782c8316a8d77f26c97da7
encio <peter.encio@gmail.com>
2022-11-06 22:53:48 +0800

    [Core] React Native paper

--- App.js
+++ App.js
@@ -6,20 +6,60 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
+import {Searchbar} from 'react-native-paper';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text style={styles.text}> Hello world</Text>
+    <SafeAreaView style={styles.main}>
+      <Search />
+      <Body />
     </SafeAreaView>
   );
 }
 
+const Search = () => {
+  const [search, searchQuery] = useState('');
+  const onChangeSearch = query => searchQuery(query);
+  console.log(search);
+  return (
+    <View style={styles.search}>
+      <Searchbar
+        placeholder="search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+    </View>
+  );
+};
+
+const Body = () => {
+  return (
+    <View style={styles.body}>
+      <Text>body</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
   text: {
     marginTop: 32,
     paddingHorizontal: 24,
+    color: 'black',
+  },
+  body: {
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    flex: 1,
+  },
+  search: {
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    padding: 16,
+    marginBottom: 5,
   },
 });
